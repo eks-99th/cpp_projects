5334fa01c57d99ccd29cfeb76d98a26da57f7bc3
Erick Bacurau <erickbacurau1989@gmail.com>
2024-06-28 14:47:26 +0200

    Missing only clean-up

--- CGPA_Calc/src/user_interface.cpp
+++ CGPA_Calc/src/user_interface.cpp
@@ -1,3 +1,6 @@
+#include <botan/hash.h>
+#include <botan/hex.h>
+
 #include "ftxui/component/component.hpp"       // for Input, Renderer, Vertical
 #include "ftxui/component/component_base.hpp"  // for ComponentBase
 #include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
@@ -5,11 +8,15 @@
 
 using namespace ftxui;
 
+std::string sha256_student_id(std::string &first_name,
+                              std::string &second_name);
+
 extern double gGPA;
 
 void render_ui() {
   std::string first_name;
   std::string last_name;
+  std::string student_id;
   std::string course_name;
   std::string hours_credits;
   int credit_hours = 0;
@@ -34,7 +41,7 @@ void render_ui() {
   Component input_first_name = Input(&first_name, "first name");
   Component input_last_name = Input(&last_name, "last name");
   Component course = Input(&course_name, "course");
-  Component input_credits = Input(&hours_credits, "course");
+  Component input_credits = Input(&hours_credits, "credits");
 
   // The component tree:
   auto component = Container::Vertical(
@@ -43,10 +50,11 @@ void render_ui() {
 
   // Tweak how the component tree is rendered:
   auto renderer = Renderer(component, [&] {
+    student_id = sha256_student_id(first_name, last_name);
     return vbox({
                hbox(text(" First name : "), input_first_name->Render()),
                hbox(text(" Last name  : "), input_last_name->Render()),
-               hbox(text(" Student Id  : ABCDEF")),
+               hbox(text(" Student Id  :" + student_id)),
                separator(),
                hbox(text(" Course Name :"), course->Render()),
                hbox(text(" Credits :"), input_credits->Render()),
@@ -68,4 +76,15 @@ void render_ui() {
 
   auto screen = ScreenInteractive::FitComponent();
   screen.Loop(renderer);
+}
+
+std::string sha256_student_id(std::string &first_name,
+                              std::string &second_name) {
+  auto str = first_name + second_name;
+  std::vector<uint8_t> buf(2048);
+  const auto hash1 = Botan::HashFunction::create_or_throw("SHA-256");
+  hash1->update(buf.data(), str.length());
+  auto sha = Botan::hex_encode(hash1->final());
+  return sha.substr(0, 5);
+  ;
 }
